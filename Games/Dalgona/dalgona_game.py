147ddbd93f6ae8433a643057e18717c1976ca454
hanum <rntls123@naver.com>
2021-11-01 00:04:58 +0900

    11.1 오한음 More strong points

--- Games/Dalgona/dalgona_game.py
+++ Games/Dalgona/dalgona_game.py
@@ -1,8 +1,7 @@
+import math
+
 import game_object
 from Games.game_settings import *
-
-COLUMN_COUNT = 3
-ROW_COUNT = 3
 
 
 class Game:
@@ -14,20 +13,21 @@ class Game:
         self.game_screen.fill(PINK)
 
     def start_game(self):
-        dot = pygame.image.load("Media/dot.png")
-        donut = pygame.image.load("Media/donut.png")
-
-        def printing():
-            print(123)
+        points = []
+        for i in range(20):
+            theta = (2 * math.pi / 20) * i
+            pos_x = self.width / 2 + 100 * math.cos(theta)
+            pos_y = self.height / 2 + 100 * math.sin(theta)
+            points.append(game_object.Point(self.game_screen, pos_x, pos_y, 5))
 
         while True:
-            # main_image = pygame.image.load()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
             pygame.draw.circle(self.game_screen, YELLOWBROWN, [self.width / 2, self.height / 2], 300, 300)
             pygame.draw.circle(self.game_screen, (175, 118, 43), [self.width / 2 + 10, self.height / 2], 220, 15)
-            game_object.Point(self.game_screen, 100, 220, 20)
+            for i in points:
+                i.punching()
             pygame.display.update()
             clock.tick(TICK_RATE)
 
--- Games/Dalgona/game_object.py
+++ Games/Dalgona/game_object.py
@@ -7,15 +7,25 @@ class Dalgona:
 
 class Point:
     def __init__(self, game_display, x, y, radius):
-        self.mouse = pygame.mouse.get_pos()
-        self.click = pygame.mouse.get_pressed()
+        self.game_display = game_display
         self.clicked = False
-        if x + radius > self.mouse[0] > x - radius and y + radius > self.mouse[1] > y - radius:
-            if self.click[0]:
+        self.radius = radius
+        self.x = x
+        self.y = y
+
+    def is_clicked(self):
+        mouse = pygame.mouse.get_pos()
+        if self.x + self.radius > mouse[0] > self.x - self.radius and self.y + self.radius > \
+                mouse[1] > self.y - self.radius:
+            if pygame.mouse.get_pressed()[0]:
                 self.clicked = True
 
+    def draw(self):
         if self.clicked:
-            pygame.draw.circle(game_display, BLACK, [x, y], 6, 5)
-
+            pygame.draw.circle(self.game_display, BLACK, [self.x, self.y], self.radius, 6)
         else:
-            pygame.draw.circle(game_display, BROWN, [x, y], 6, 2)
+            pygame.draw.circle(self.game_display, BROWN, [self.x, self.y], self.radius, 2)
+
+    def punching(self):
+        self.is_clicked()
+        self.draw()
