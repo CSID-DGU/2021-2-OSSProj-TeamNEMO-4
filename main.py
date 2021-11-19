f8100292845af43f129a1d880d18a29f1c01f354
hanum <rntls123@naver.com>
2021-11-19 20:11:05 +0900

    11.19 오한음 Best record mode

--- main.py
+++ main.py
@@ -40,22 +40,45 @@ if __name__ == "__main__":
                 break
 
             LEVEL += 1
+    elif selected == "the_best_record_mode":
+        while True:
+            mugunghwa_score = start_mugunghwa_game(LEVEL, SCORE)
+            if mugunghwa_score:
+                SCORE += mugunghwa_score
+            else:
+                break
+            dalgona_score = start_dalgona_game(LEVEL, SCORE)
+            if dalgona_score:
+                SCORE += dalgona_score
+            else:
+                break
+            marble_score = start_marble_game(LEVEL, SCORE)
+            if marble_score:
+                SCORE += marble_score
+            else:
+                break
+            tug_score = start_tug_game(LEVEL, SCORE, best_record_mode=True)
+            if tug_score:
+                SCORE += tug_score
+            else:
+                break
+            break
 
-        pygame.init()
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        ref_w, ref_h = screen.get_size()
-        RUNNING = True
-        while RUNNING:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    RUNNING = False
-            screen.fill(PINK)
-            message_to_screen_center(screen, '게임 종료', WHITE, korean_font,
-                                     SCREEN_WIDTH / 3,
-                                     ref_w,
-                                     ref_h)
-            message_to_screen_center(screen, f'점수는 {round(SCORE)} 점입니다. ', WHITE, korean_font,
-                                     SCREEN_WIDTH / 2,
-                                     ref_w,
-                                     ref_h)
-            pygame.display.update()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    ref_w, ref_h = screen.get_size()
+    RUNNING = True
+    while RUNNING:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUNNING = False
+        screen.fill(PINK)
+        message_to_screen_center(screen, '게임 종료', WHITE, korean_font,
+                                 SCREEN_WIDTH / 3,
+                                 ref_w,
+                                 ref_h)
+        message_to_screen_center(screen, f'점수는 {round(SCORE)} 점입니다. ', WHITE, korean_font,
+                                 SCREEN_WIDTH / 2,
+                                 ref_w,
+                                 ref_h)
+        pygame.display.update()
