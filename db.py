c6371dbf8a66014566686173ddaba618ec50c593
hanum <rntls123@naver.com>
2021-11-24 19:45:53 +0900

    11.24 오한음 Success to connect DB cloud !

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
+import certifi
 from pymongo import MongoClient
 
 URI = "mongodb+srv://ossproj.32z9y.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
 client = MongoClient(URI,
                      tls=True,
-                     tlsCertificateKeyFile='X509-cert-174560122426641057.pem')
-db = client['testDB']
-collection = db['testCol']
-print(collection)
-# doc_count = collection.count_documents({})
-# print(doc_count)
+                     tlsCertificateKeyFile='X509-cert-174560122426641057.pem',
+                     tlsCAFile=certifi.where()
+                     )
+
+db = client['OSSProj']
+collection = db['testing']
+print(collection.insert_one({"hello": "testing"}))
